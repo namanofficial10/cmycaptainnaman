dd9fa4b4389db4beb5365c5ce7d77b1defae0b72
namanofficial10 <75782506+namanofficial10@users.noreply.github.com>
2021-02-01 15:31:30 +0530

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+  int food;
+  
+  printf("Enter any number between 1 to 5:");
+  scanf(%d,&food)
+  
+  switch(food){
+    case 1:
+      print("Food Item- Pizza \nPrice- Rs.239)
+      break;
+    case 2:
+      print("Food Item- Burger \nPrice- Rs.129)
+      break;
+    case 3:
+      print("Food Item- Sandwich \nPrice- Rs.149)
+      break;
+    case 4:
+      print("Food Item- Pasta \nPrice- Rs.179)
+      break;
+    default:
+      print("Food Item- French Fries \nPrice- Rs.99)
+      break;
+    
+  }
+  
+  return 0;
+}
